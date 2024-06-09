ae9b8014239143149ff0044b5ee66f6883b20e4f
sunguk <111402029+supark8249@users.noreply.github.com>
2024-06-09 22:48:57 +0900

    Update visitor.js

--- routes/visitor.js
+++ routes/visitor.js
@@ -45,14 +45,20 @@ router.get('/', function (req, res, next) {
 	// return res.render('visitor.ejs', {"visitor":visitor});
 });
 
+router.get('/visitorNew', function (req, res, next) {
+	console.log("visitorNew");
+
+	return res.render('visitorNew.ejs');
+});
+
 router.get('/visitorEdit', async (req, res) => {
+	console.log("visitorEdit");
 	try {
 		const vid = req.query.vid;
 
 		const vEdit = await visitor.findOne({ id: vid });
 	
 		if (!vEdit) {
-			// handle case where board is not found (e.g., send 404 error)
 			return res.status(404).send('visitors not found');
 		}
 	
@@ -63,16 +69,12 @@ router.get('/visitorEdit', async (req, res) => {
 	}
 });
 
-router.post('/visitorSave', async (req, res) => {
+router.post('/visitorUpdate', async (req, res) => {
+	console.log("visitorUpdate");
 	try {
-	  	console.log("visitorSave post: ", req.body.id, req.body.content);
+	  	console.log("visitorUpdate post: ", req.body.id, req.body.content);
   
 	  	const vid = req.body.id;
-	  	console.log("req.body.id:", req.body.id);
-	  	console.log("req.body.vid:", vid);
-	  	console.log("req.body.content:", req.body.content);
-	  	console.log("req.body.password:", req.body.password);
-	  	console.log("req.body.password_o:", req.body.password_o);
   
 		if (!vid) {
 			return res.status(400).json({
@@ -102,7 +104,7 @@ router.post('/visitorSave', async (req, res) => {
 			});
 		}
   
-	  	res.redirect('/');
+	  	res.redirect('/visitor');
 	} catch (error) {
 	 	console.error(error);
 	  	res.status(500).json({
@@ -112,145 +114,27 @@ router.post('/visitorSave', async (req, res) => {
 	}
   });
   
-  
-// router.post('/register', function(req, res, next) {
-// 	console.log(req.body);
-// 	var personInfo = req.body;
 
-
-// 	if(!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf){
-// 		res.send();
-// 	} else {
-// 		if (personInfo.password == personInfo.passwordConf) {
-
-// 			User.findOne({email:personInfo.email},function(err,data){
-// 				if(!data){
-// 					var c;
-// 					User.findOne({},function(err,data){
-
-// 						if (data) {
-// 							console.log("if");
-// 							c = data.unique_id + 1;
-// 						}else{
-// 							c=1;
-// 						}
-
-// 						var newPerson = new User({
-// 							unique_id:c,
-// 							email:personInfo.email,
-// 							username: personInfo.username,
-// 							password: personInfo.password,
-// 							passwordConf: personInfo.passwordConf
-// 						});
-
-// 						newPerson.save(function(err, Person){
-// 							if(err)
-// 								console.log(err);
-// 							else
-// 								console.log('Success');
-// 						});
-
-// 					}).sort({_id: -1}).limit(1);
-// 					res.send({"Success":"You are regestered,You can login now."});
-// 				}else{
-// 					res.send({"Success":"Email is already used."});
-// 				}
-
-// 			});
-// 		}else{
-// 			res.send({"Success":"password is not matched"});
-// 		}
-// 	}
-// });
-
-// router.get('/login', function (req, res, next) {
-// 	return res.render('login.ejs');
-// });
-
-// router.get('/register', function (req, res, next) {
-// 	return res.render('register.ejs');
-// });
-
-// router.post('/login', function (req, res, next) {
-// 	//console.log(req.body);
-// 	User.findOne({email:req.body.email},function(err,data){
-// 		if(data){
-			
-// 			if(data.password==req.body.password){
-// 				//console.log("Done Login");
-// 				req.session.userId = data.unique_id;
-// 				req.session.username = data.username;
-// 				//console.log(req.session.userId);
-// 				res.send({"Success":"Success!"});
-// 			        //res.render('loghome.ejs', {"name":data.username,"email":data.email});
-				
-// 			}else{
-// 				res.send({"Success":"Wrong password!"});
-// 			}
-// 		}else{
-// 			res.send({"Success":"This Email Is not regestered!"});
-// 		}
-// 	});
-// });
-
-// router.get('/profile', function (req, res, next) {
-// 	console.log("profile");
-// 	User.findOne({unique_id:req.session.userId},function(err,data){
-// 		console.log("data");
-// 		console.log(data);
-// 		if(!data){
-// 			res.redirect('/');
-// 		}else{
-// 			//console.log("found");
-// 			return res.render('profile.ejs', {"name":data.username,"email":data.email});
-// 		}
-// 	});
-// });
-
-// router.get('/logout', function (req, res, next) {
-// 	console.log("logout")
-// 	if (req.session) {
-//     // delete session object
-//     req.session.destroy(function (err) {
-//     	if (err) {
-//     		return next(err);
-//     	} else {
-//     		return res.redirect('/');
-//     	}
-//     });
-// }
-// });
-
-// router.get('/forgetpass', function (req, res, next) {
-// 	res.render("forget.ejs");
-// });
-
-// router.post('/forgetpass', function (req, res, next) {
-// 	//console.log('req.body');
-// 	//console.log(req.body);
-// 	User.findOne({email:req.body.email},function(err,data){
-// 		console.log(data);
-// 		if(!data){
-// 			res.send({"Success":"This Email Is not regestered!"});
-// 		}else{
-// 			// res.send({"Success":"Success!"});
-// 			if (req.body.password==req.body.passwordConf) {
-// 			data.password=req.body.password;
-// 			data.passwordConf=req.body.passwordConf;
-
-// 			data.save(function(err, Person){
-// 				if(err)
-// 					console.log(err);
-// 				else
-// 					console.log('Success');
-// 					res.send({"Success":"Password changed!"});
-// 			});
-// 		}else{
-// 			res.send({"Success":"Password does not matched! Both Password should be same."});
-// 		}
-// 		}
-// 	});
+  router.post('/visitorSave', async (req, res) => {
+	console.log("visitorSave");
+	try {
+		const id = req.body.id;
+		const content = req.body.content;
+		const password = req.body.password;
 	
-// });
-
+		const newVisitor = new visitor({
+		  id: id,
+		  content,
+		  password,
+		});
+	
+		await newVisitor.save();
+	
+		res.redirect("/visitor");
+	  } catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Internal Server Error' });
+	  }
+  });
+  
 module.exports = router;
